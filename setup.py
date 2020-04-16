ee2bf393265d2b99e923093f397507dff4e66d68
Andre Frade <42149567+apfrade@users.noreply.github.com>
2020-04-16 12:42:54 +0100

    changed setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r") as readme_file:
+with open("README.md", encoding="utf8") as readme_file:
     long_description = readme_file.read()
 
 requirements = ["pandas>=0.2", "numpy>=1", matplotlib]
